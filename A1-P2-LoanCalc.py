c571726dd2e7e0d5e101873e2ca3c23af2fe0e1a
apb-nscc <W0487099@campus.nscc.ca>
2024-09-25 16:14:06 -0300

    Ready for Marking

--- A1-P2-LoanCalc.py
+++ A1-P2-LoanCalc.py
@@ -5,15 +5,22 @@ def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
     #Display Program Name
+    print("Weekly Loan Calculator\n")
 
     #Enter the amount of the loan
+    loan_amount = float(input("Enter the amount of the loan: "))
 
     #Enter the interest rate (%)
+    interest_rate = float(input("Enter the interest rate (%): "))
 
     #Enter the number of years
+    years = int(input("Enter the number of years: "))
+    print("")
 
     #Display total numbers of years
-
+    i = interest_rate / 5200 #using i from the formula in the assisnment 1 document for program two
+    weekly_payment = (i / (1-(1+i)**(-52*years))) * loan_amount
+    print("Your weekly payment will be: ${0:.2f}".format(weekly_payment))
     
 
 
